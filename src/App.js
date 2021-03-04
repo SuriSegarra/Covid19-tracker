c78812c69ee3f11d4244b76cd107445bae728aa0
Surelis Segarra <sdarianne34@gmail.com>
2021-03-03 18:30:24 -0600

    now you can see real data of countries on browser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { FormControl, Select, MenuItem } from '@material-ui/core';
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent,
+} from '@material-ui/core';
 import InfoBox from './InfoBox';
+import Map from './Map';
 import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
   // useEffect = runs piece of code based on given condition
   // the code will run once
   // when component loads and not again after
@@ -28,40 +36,79 @@ function App() {
     getCountriesData();
   }, []);
 
-  const onCountryChange = (e) => {
+  const onCountryChange = async (e) => {
     const countryCode = e.target.value;
 
     setCountry(countryCode);
-  };
+    const url =
+      countryCode === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19/all'
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        // all of the dta from the country response
+        setCountryInfo(data);
+      });
+  };
+  console.log('country info', countryInfo);
   return (
     <div className='app'>
-      <div className='app__header'>
-        {/* header */}
-        <h1> COVID-19 TRACKER</h1>
-        {/* dropdown */}
-        <FormControl className='app_dropdown'>
-          <Select variant='outlined' onChange={onCountryChange} value={country}>
-            <MenuItem value='worldwide'>Worldwide</MenuItem>
-            {/* Loop through all the countries and show the dropdown of those countries */}
+      <div className='app__left'>
+        <div className='app__header'>
+          {/* header */}
+          <h1> COVID-19 TRACKER</h1>
+          {/* dropdown */}
+          <FormControl className='app_dropdown'>
+            <Select
+              variant='outlined'
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value='worldwide'>Worldwide</MenuItem>
+              {/* Loop through all the countries and show the dropdown of those countries */}
 
-            {countries.map((country) => (
-              <MenuItem value={country.value}> {country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+              {countries.map((country) => (
+                <MenuItem value={country.value}> {country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        {/* Info boxes */}
+        <div className='app__stats'>
+          <InfoBox
+            title='Coronavirus cases'
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title='Recovered'
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title='Deaths'
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+
+        {/* Table  */}
+        {/* Graph */}
+
+        {/* Map */}
+        <Map />
       </div>
-      {/* Info boxes */}
-      <div className='app__stats'>
-        <InfoBox title='Coronavirus cases' cases={123} total={200} />
-        <InfoBox title='Recovered' cases={1234} total={3000} />
-        <InfoBox title='Deaths' cases={12345} total={4000} />
-      </div>
-
-      {/* Table  */}
-      {/* Graph */}
-
-      {/* Map */}
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          {/* Table */}
+          <h3>Worldwide New Cases</h3>
+          {/* Graph */}
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/Map/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function index() {
+  return (
+    <div className='map'>
+      <h1> I am a map</h1>
+    </div>
+  );
+}
+
+export default index;
